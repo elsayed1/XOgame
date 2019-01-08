7b6db22d43295691d813dd5cca3f8a8ddf524159
elsayed1 <elsayed_alaa70@yahoo.com>
2019-01-08 22:11:59 +0200

    v.01

--- src/components/xoGame/xoGame.jsx
+++ src/components/xoGame/xoGame.jsx
@@ -24,9 +24,135 @@ class Game extends React.Component {
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    console.log(history);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+  makeComputerPlay(squares) {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+      arr.push([]);
+      for (let j = 0; j < 3; j++)
+        arr[i][j] =
+          squares[i * 3 + j] === "X" ? 1 : squares[i * 3 + j] === "O" ? 2 : 0;
+    }
+    let computerPos = this.solve(arr) - 1;
+    console.log(computerPos);
+    this.computerClick(computerPos);
+  }
+  // Solve Function
+  solve(arr) {
+    let bestPath = new Array(3);
+    let score = 0,
+      penalty = 0,
+      solutionCell = 0;
+    if (arr[1][1] == 0) return 5;
+    for (let i = 0; i < 3; i++) {
+      penalty = 0;
+      let s = 0;
+      for (let j = 0; j < 3; j++)
+        if (arr[i][j] === 0) ++s;
+        else if (arr[i][j] === 2) s += 2;
+        else ++penalty;
+      if (penalty === 2 && s === 1) {
+        for (let j = 0; j < 3; j++)
+          if (arr[i][j] === 0) solutionCell = i * 3 + j + 1;
+      }
+      if (s > score && s !== 4 && s !== 4) {
+        score = s;
+        bestPath[0] = i * 3 + 1;
+        bestPath[1] = i * 3 + 2;
+        bestPath[2] = i * 3 + 3;
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      penalty = 0;
+      let s = 0;
+      for (let j = 0; j < 3; j++)
+        if (arr[j][i] === 0) ++s;
+        else if (arr[j][i] === 2) s += 2;
+        else ++penalty;
+      if (penalty === 2 && s === 1) {
+        for (let j = 0; j < 3; j++)
+          if (arr[j][i] == 0) solutionCell = j * 3 + i + 1;
+      }
+      if (s > score && s !== 4) {
+        score = s;
+        bestPath[0] = 0 * 3 + i + 1;
+        bestPath[1] = 1 * 3 + i + 1;
+        bestPath[2] = 2 * 3 + i + 1;
+      }
+    }
+    let s = 0;
+    penalty = 0;
+    for (let i = 0; i < 3; i++) {
+      if (arr[i][i] == 0) ++s;
+      else if (arr[i][i] == 2) s += 2;
+      else ++penalty;
+    }
+    if (penalty == 2 && s === 1) {
+      for (let j = 0; j < 3; j++)
+        if (arr[j][j] == 0) solutionCell = j * 3 + j + 1;
+    }
+    if (s > score && s !== 4) {
+      score = s;
+      bestPath[0] = 1;
+      bestPath[1] = 5;
+      bestPath[2] = 9;
+    }
+    s = 0;
+    penalty = 0;
+    for (let i = 0; i < 3; i++) {
+      if (arr[i][2 - i] == 0) ++s;
+      else if (arr[i][2 - i] == 2) s += 2;
+      else ++penalty;
+    }
+    if (penalty == 2 && s === 1) {
+      for (let j = 0; j < 3; j++)
+        if (arr[j][2 - j] == 0) solutionCell = j * 3 + (2 - j) + 1;
+    }
+    if (s > score && s !== 4) {
+      score = s;
+      bestPath[0] = 3;
+      bestPath[1] = 5;
+      bestPath[2] = 7;
+    }
+    if (score === 5) {
+      for (let i = 0; i < 3; i++) {
+        let row = Math.ceil(bestPath[i] / 3) - 1;
+        let column = Math.trunc(bestPath[i] - row * 3 - 1);
+        console.log(bestPath);
+        console.log(row, column);
+        if (arr[row][column] === 0) return bestPath[i];
+      }
+    } else if (solutionCell != 0) {
+      return solutionCell;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        let row = Math.ceil(bestPath[i] / 3) - 1;
+        let column = Math.ceil(bestPath[i] - row * 3 - 1);
+        console.log(row, column, bestPath);
+        if (arr[row][column] === 0) return bestPath[i];
+      }
+    }
+  }
+  computerClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    console.log(squares);
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -50,8 +176,10 @@ class Game extends React.Component {
   }
 
   render() {
-    const history = this.state.history;
+    const { history } = this.state;
     const current = history[this.state.stepNumber];
+    if (!this.state.xIsNext && this.state.stepNumber < 9)
+      this.makeComputerPlay(current.squares);
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -65,9 +193,9 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner: " + (winner === "X" ? winner : "Computer");
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = "Next player: " + (this.state.xIsNext ? "X" : "Computer");
     }
 
     return (
